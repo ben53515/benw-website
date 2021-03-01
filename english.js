c7f808d764c822ae893974e36be90900a359516e
ben53515 <63850882+ben53515@users.noreply.github.com>
2021-03-01 19:44:44 +0000

    more english resources

--- english.js
+++ english.js
@@ -1,6 +1,11 @@
 let learningChannels = [
     { name: "Deliberate English", url: "https://www.youtube.com/c/DeliberateEnglish", description: "Great channel for intermediate students that want quick progress in listening comprehension and speaking fluency"},
-    { name: "English Pronunciation Roadmap", url: "https://www.youtube.com/channel/UCIeRsxgia3oNEuKAuXEPh0A", description: "For those who want to improve their pronunciation and sound very natural when speaking"}
+    { name: "English Pronunciation Roadmap", url: "https://www.youtube.com/channel/UCIeRsxgia3oNEuKAuXEPh0A", description: "For those who want to improve their pronunciation and sound very natural when speaking"},
+    { name: "Closely Observed English", url: "https://www.youtube.com/c/CloselyObservedEnglish", description: "Focused on differences between formal and informal english, particularly in the written language"},
+    { name: "Papa Teach Me", url: "https://www.youtube.com/c/papateachme", description: "Focused largely on pronunciation, with some expressions and vocabulary included too"},
+    { name: "English with Benjamin", url: "https://www.youtube.com/c/EngvidBenjamin", description: "Covers wide range of topics, largely grammar and vocabulary"},
+    { name: "Go Natural English", url: "https://www.youtube.com/c/GoNaturalEnglish", description: "American English, vocabulary and fluency tips"},
+    { name: "ETJ English", url: "https://www.youtube.com/c/ETJEnglish", description: "focused on sounding more natural with pronunciation, intonation and expressions"}
     //{ name: "", url: "", description: ""},
 
   ];
