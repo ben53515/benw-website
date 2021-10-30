a444e44f392b00b34c42b387c4a6fd1082e82d06
ben53515 <ben@netscrape.co.uk>
2021-10-30 11:20:53 +0100

    updated spanish and french resources

--- french.js
+++ french.js
@@ -13,12 +13,16 @@ let learningChannels = [
   { name: "Alice Ayel", url: "https://www.youtube.com/channel/UCbC9Utt1c6UXRqWw2lOTCBg", description: "Amazing stories and comprehensible input for complete beginners and up, very useful"},
   { name: "French with Tony", url: "https://www.youtube.com/channel/UC-8POjZ30a3YD63Gys0wWNA", description: "Great channel with content for beginners and intermediate with stories and also authentic conversations"},
   { name: "Julie Alors", url: "https://www.youtube.com/channel/UCVJJy6MLOtSJQebC86L7SDw", description: "Great channel with videos for all levels of comprehensible input"},
-  { name: "Français Authentique", url: "https://www.youtube.com/channel/UCQpM25U6iqaRSO-SZxd5oDw", description: "Great channel for intermediate levels and above with discussions about a variety of topics"},
-  { name: "Français avec Pierre", url: "https://www.youtube.com/channel/UCVgW9ZQaGBk6fsiPgE2mYDg", description: "Great channel for grammar explanations in slow, clear french"},
   { name: "French Comprehensible Input", url: "https://www.youtube.com/channel/UC-XUpEBvcQcRqMdtLhoXmOA", description: "interesting and engaging videos for all levels, comprehensible input"},
+  { name: "Français avec Pierre", url: "https://www.youtube.com/channel/UCVgW9ZQaGBk6fsiPgE2mYDg", description: "Great channel for grammar explanations in slow, clear french"},
   { name: "Inner French", url: "https://www.youtube.com/channel/UCI4xp8qHD1MDErkqxb1dPbA", description: "Slow, clear french with a variety of topics for intermediate students"},
+  { name: "Français Authentique", url: "https://www.youtube.com/channel/UCQpM25U6iqaRSO-SZxd5oDw", description: "Great channel for intermediate levels and above with discussions about a variety of topics"},
+  { name: "Guide to French Pronunciation", url: "https://www.youtube.com/channel/UCZ0FVpygzWPgtpWS_-ENTsg", description: "Explanations of how to pronounce french sounds with mouth positions etc - very useful! (videos in english)"},
+  { name: "Elma dit", url: "https://www.youtube.com/c/Elmadit", description: "Explanations of pronunciation and some other topics, videos in french or portuguese"},
+  { name: "Piece of French", url: "https://www.youtube.com/c/PieceofFrench", description: "Slow, clear french with subtitles, everyday topics"},
+  { name: "François le français", url: "https://www.youtube.com/channel/UCvnjBpG2GikgvHB7qrRG5Yw", description: "Explanations of songs/videos, learn slang"},
   { name: "Extra en Français", url: "https://www.youtube.com/playlist?list=PLhAxKCm7GPvi1fmbLUbhYhQcLGTZ2Fqwq", description: "Program Made for students of French, so stupid its hilarious but also useful to learn from if you're a beginner"}
-
+  
 ];
 let learningChannelsCode = ``;
 learningChannels.forEach(learningChannel => {
@@ -29,11 +33,24 @@ learningChannels.forEach(learningChannel => {
 let channels = [
     { name: "Le Monde des Langues", url: "https://www.youtube.com/channel/UC1ipKNAWcHhmcqKUEGg1RMg", description: "Content aimed at french speakers, discussing languages and other interesting topics"},
     { name: "Lauriane- Marathon des Langues", url: "https://www.youtube.com/channel/UCF6Z8cpFJke-s9mqsGkDK8Q", description: "Content aimed at french speakers, discussing languages and other interesting topics"},
+    { name: "Linguisticae", url: "https://www.youtube.com/c/Linguisticae", description: "Very fun, speaks about linguistics, speaks fast"},
+    { name: "Les langues de Cha", url: "https://www.youtube.com/c/LesLanguesdeCha", description: "Linguistics, informative and useful"},
+    { name: "Apprendre une langue", url: "https://www.youtube.com/c/apprendreunelangue", description: "Speaks about language learning, particularly how to start quickly. Comprehensible videos"},
     { name: "Claire et Arthur Les droners", url: "https://www.youtube.com/channel/UCSmifVtM_TtxUCRrPQ_yqKw", description: "Discusses travel, photography and the environment"},
-    { name: "Mathias", url: "https://www.youtube.com/channel/UCOLk9tXeX8QI66ZYeW_vO-g", description: "Personal development, sport, willpower, challenges"},
+    { name: "Hugo Décrypte", url: "https://www.youtube.com/c/HugoD%C3%A9crypte", description: "Weekday short videos with news about France or international subjects"},
+    { name: "France Culture", url: "https://www.youtube.com/user/FranceCulture", description: "Videos about various subjects related to France and the french language"},
     { name: "Kaizen Lane", url: "https://www.youtube.com/channel/UCCVy4FTw__pk6bvZsPyTSLg", description: "Personal development, study tips, productivity"},
-    { name: "M4RCO", url: "https://www.youtube.com/channel/UCmYC3JFfcXEUNUUcJH4ZQRQ", description: "Well made, informative videos about Astronomy, Aerospace and Space"}
-
+    { name: "Mathias", url: "https://www.youtube.com/channel/UCOLk9tXeX8QI66ZYeW_vO-g", description: "Personal development, sport, willpower, challenges"},
+    { name: "Cyrus North", url: "https://www.youtube.com/c/CyrusNorth", description: "Psychology, thoughts about tough topics"},
+    { name: "Fabien Olicard", url: "https://www.youtube.com/c/FabienOlicard", description: "Mentalism (illusions, brain and mental biases etc)"},
+    { name: "Science Etonnante", url: "https://www.youtube.com/c/ScienceEtonnante", description: "Scientific videos, mainly Physics, explained clearly and enjoyably"},
+    { name: "Dr Nozman", url: "https://www.youtube.com/c/DrNozman", description: "Science and interesting subjects at an entry level"},
+    { name: "Dirty Biology", url: "https://www.youtube.com/c/dirtybiology", description: "Science videos about biology or other interesting things"},
+    { name: "Fouloscopie", url: "https://www.youtube.com/c/Fouloscopie", description: "une foule = a crowd, the science of crowds - very interesting and unique"},
+    { name: "M4RCO", url: "https://www.youtube.com/channel/UCmYC3JFfcXEUNUUcJH4ZQRQ", description: "Well made, informative videos about Astronomy, Aerospace and Space"},
+    { name: "Cyprien", url: "https://www.youtube.com/c/cyprien", description: "Short comedic videos and sketches about various topics"},
+    { name: "Paul Taylor's Happy Hour", url: "https://www.youtube.com/c/PaulTaylorsHappyHourLive", description: "English comedian who speaks very good french talks to invitees casually and comically (in english and french)"}
+    
   ];
 
 
--- spanish.js
+++ spanish.js
@@ -17,10 +17,11 @@ let learningChannels = [
   { name: "NachoTime Spanish", url: "https://www.youtube.com/channel/UCzr7FsXkETYaaHTySIeJsjQ", description: "Also an Amazing Channel, related to Website as mentioned but also provides challenging interactive videos for intermediate level+ that really help you to improve"},
   { name: "Bilingüe Blogs", url: "https://www.youtube.com/channel/UCa57CLZlqhFtn1m-e-tWhpg", description: "Very different kind of Spanish spoken here with Dominican Spanish, gives out great tips and I recommend the Listening Podcast-style Videos, recommend for all levels, listening for intermediate+"},
   { name: "Español con Juan", url: "https://www.youtube.com/channel/UCoHJ7PkM6T92LwgJgrnDhWA", description: "Amazing grammar explanations, very entertaining the whole time, explanations with full context so if you're looking for something explained come here, recommend for upper-beginners and intermediate+"},
+  { name: "Linguriosa", url: "https://www.youtube.com/c/Linguriosa", description: "Fun videos to learn difficult concepts or learn about culture and sometimes etymology etc"},
   { name: "SpanishPodcast.net", url: "https://www.youtube.com/channel/UCpfjMrPcCxl6aP7qIkN8Jpg", description: "Linked to aforementioned website, videos with awesome tips for learning and explanations, recommend for intermediate+"},
   { name: "Why Not Spanish?", url: "https://www.youtube.com/channel/UCIdFcLCIJQ_YMrormG_nU8w", description: "Another different accent here (Colombian), very clear explanations and well formatted videos to help you learn vocab, for all levels, recommend the listening activity videos especially"},
-  { name: "Extra en Español", url: "https://www.youtube.com/watch?v=Dfb9-ZTCA-E&list=PLLVULqsElvki_riI1ld0QhZJx1wom5Ply&index=1", description: "Program Made for students of spanish, so stupid its hilarious but also useful to learn from if you're a beginner"}
-  
+  { name: "Extra en Español", url: "https://www.youtube.com/watch?v=Dfb9-ZTCA-E&list=PLLVULqsElvki_riI1ld0QhZJx1wom5Ply&index=1", description: "Program Made for students of spanish, so stupid its hilarious but also useful to learn from if you're a beginner"},
+  //{ name: "", url: "", description: ""},
 ];
 let learningChannelsCode = ``;
 learningChannels.forEach(learningChannel => {
@@ -31,17 +32,22 @@ learningChannels.forEach(learningChannel => {
 let channels = [
   { name: "Kike Arnaiz", url: "https://www.youtube.com/channel/UCV__GhTQCk6sfTwmKQ5ahxg", description: "Makes Travel Videos, large range of countries, amazing quality photography and videos"},
   { name: "VAGABOOM", url: "https://www.youtube.com/channel/UCPgrd83e1TOKFs46JdFu4Gg", description: "Also travel videos, Mexican Spanish and generally very interesting"},
+  { name: "Lethal Crysis", url: "https://www.youtube.com/c/lethalcrysischannel", description: "Extreme travel ie to everywhere you want to see but don't dare go"},
+  { name: "Beyond Words", url: "https://www.youtube.com/c/beyondwords", description: "Personal development, casual conversations, relax"},
   { name: "El Jardín de Martín", url: "https://www.youtube.com/channel/UCpOwrlKnpgfpqzr294zy-ow", description: "Peruvian living in Germany, interesting language learning tips"},
+  { name: "Mr Salas", url: "https://www.youtube.com/c/MrSalas", description: "Mexican who teaches english, great ideas for learning techniques"},
+  { name: "Blog de Lengua", url: "https://www.youtube.com/c/BlogdeLengua", description: "Spanish grammar, vocabulary etc but made for natives, advanced topics"},
   { name: "Hemisferio Derecho", url: "https://www.youtube.com/channel/UCo6CWGOfDeNTmpATA-k2tSA", description: "Also makes Personal Development Videos and makes great animations"},
-  { name: "Secretos de la Vida", url: "https://www.youtube.com/channel/UCcyeS-u6ErEO-Az7J56DsIg", description: "Very entertaining videos on personal development, great voice and accent to mimic"},
   { name: "La Gata de Schrödinger", url: "https://www.youtube.com/channel/UCoXtmmnLCbXDiSo8GxsmOzA", description: "Videos about science and other interesting topics such as debunking conspiracy theories and false beliefs"},
   { name: "Quantum Fracture", url: "https://www.youtube.com/channel/UCbdSYaPD-lr1kW27UJuk8Pw", description: "Talks about scientific topics mainly Physics and Engineering related in an engaging format"},
   { name: "Píldoras de Psicología", url: "https://www.youtube.com/channel/UCKJNeVvjAI25bHQUYFuNVwQ", description: "Talks about Psychology and other scientific topics, very informative"},
   { name: "Date un Vlog", url: "https://www.youtube.com/channel/UCQX_MZRCaluNKxkywkLEgfA", description: "Great channel about all things scientific, very clear spoken and great explanations"},
+  { name: "Jaime Altozano", url: "https://www.youtube.com/c/JaimeAltozano", description: "Musician who shares interesting things related to music"},
+  { name: "Hora del coto", url: "https://www.youtube.com/c/HoraDelcoto", description: "Mexican spanish, reactions to spanish comedians and more"},
   { name: "La Resistencia", url: "https://www.youtube.com/channel/UCvosUrZ7hXpzAyobhfztg4w", description: "Clips from a Spanish Talk show/Podcast-style videos where many celebrities are interviewed, very natural conversational spanish"},
   { name: "The Wild Project", url: "https://www.youtube.com/channel/UCBYyJBCtCvgqA4NwtoPMwpQ", description: "Spanish Podcast with informative videos about a range of topics whilst interviewing experts in different fields"},
-  { name: "Fortfast WTF", url: "https://www.youtube.com/channel/UChN3MPAc_4Qa-xkM0ZQiVbg", description: "Natural Spanish with a variety of accents talking about relevant topics and sharing interesting information"}
-
+  { name: "Fortfast WTF", url: "https://www.youtube.com/channel/UChN3MPAc_4Qa-xkM0ZQiVbg", description: "Natural Spanish with a variety of accents talking about relevant topics and sharing interesting information"},
+  
 ];
 let channelsCode = ``;
 channels.forEach(channel => {
